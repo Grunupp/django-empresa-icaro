19e02431fc8485947c867a9120eebf1181814464
Ícaro Grunupp <icarogrunupp@hotmail.com>
2025-11-12 19:10:24 -0300

    Delete django-empresa-icaro directory

--- django-empresa-icaro/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post
-# Register your models here.
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('titulo','criado_em')
